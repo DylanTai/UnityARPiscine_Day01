62ef1904bcc78dabf2b45ad25bccb0bea6ac6021
Dylan Tai <dylztai@gmail.com>
2020-02-10 15:52:01 -0800

    ex00

---
+++ Camera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Camera : MonoBehaviour
+{
+    private int which_player;
+    [SerializeField] GameObject obj_Claire;
+    [SerializeField] float Claire_speed;
+    [SerializeField] GameObject obj_John;
+    [SerializeField] float John_speed;
+    [SerializeField] GameObject obj_Thomas;
+    [SerializeField] float Thomas_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        which_player = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        obj_Claire.GetComponent<playerScript_ex00>().moveable = false;
+        obj_John.GetComponent<playerScript_ex00>().moveable = false;
+        obj_Thomas.GetComponent<playerScript_ex00>().moveable = false;
+        if (Input.GetKeyDown("1"))
+            which_player = 1;
+        if (Input.GetKeyDown("2"))
+            which_player = 2;
+        if (Input.GetKeyDown("3"))
+            which_player = 3;
+        if (which_player == 1)
+        {
+            this.transform.position = new Vector3(obj_Claire.transform.position.x, obj_Claire.transform.position.y, -10);
+            obj_Claire.GetComponent<playerScript_ex00>().speed = Claire_speed;
+            obj_Claire.GetComponent<playerScript_ex00>().moveable = true;
+        }
+        if (which_player == 2)
+        {
+            this.transform.position = new Vector3(obj_John.transform.position.x, obj_John.transform.position.y, -10);
+            obj_John.GetComponent<playerScript_ex00>().speed = John_speed;
+            obj_John.GetComponent<playerScript_ex00>().moveable = true;
+        }
+        if (which_player == 3)
+        {
+            this.transform.position = new Vector3(obj_Thomas.transform.position.x, obj_Thomas.transform.position.y, -10);
+            obj_Thomas.GetComponent<playerScript_ex00>().speed = Thomas_speed;
+            obj_Thomas.GetComponent<playerScript_ex00>().moveable = true;
+        }
+    }
+}
---
+++ playerScript_ex00.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript_ex00 : MonoBehaviour
+{
+    public bool moveable;
+    public float speed;
+    [SerializeField] float up_velocity;
+    [SerializeField] bool jump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveable = false;
+        jump = false;
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 8)
+            jump = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.layer == 8)
+            jump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moveable && jump && Input.GetKeyDown("space"))
+            this.GetComponent<Rigidbody2D>().AddForce(Vector3.up * up_velocity);
+        if (Input.GetKey("left") && moveable)
+            transform.Translate(Vector3.left * speed);
+        if (Input.GetKey("right") && moveable)
+            transform.Translate(Vector3.right * speed);
+    }
+}
